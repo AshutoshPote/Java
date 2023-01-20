a72bc4200423eb4d893cd8e5f16013bf6fa0ffc8
AshutoshPote <123161129+AshutoshPote@users.noreply.github.com>
2023-01-20 18:04:28 +0530

    Add files via upload

---
+++ Practical/hello.java
@@ -0,0 +1,15 @@
+class My{
+	public static void display()
+	{
+		System.out.println("Hello World....");
+}	
+}
+
+class Hello{
+	public static void main(String args[])
+	{
+		My.display();
+		System.out.println("Its my first program");
+		
+	}
+}
